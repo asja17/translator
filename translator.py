140bf073f42d6b14131731726e80303fd400759d
Asim Jarrar <asim@kth.se>
2024-01-04 19:05:30 +0100

    ny

--- translator.py
+++ translator.py
@@ -1,1 +1,22 @@
 import googletrans
+from googletrans import Translator
+
+#print (googletrans.LANGUAGES)
+
+
+text1 = "This is my first channel"
+
+text2 = "Detta är min första kanal"
+
+text3 = "Das sind in deutsche"
+
+transobjekt = Translator()
+
+print (transobjekt.detect(text1))
+print (transobjekt.detect(text2))
+print (transobjekt.detect(text3))
+
+
+
+print ("Översatt från svenska till engelska: ", transobjekt.translate(text2))
+
